444502c660c3fcd130aa3d97c47fa3acd17b5b75
Demilade01 <demiladeakinyemi2004@gmail.com>
2025-05-11 17:25:03 -0400

    feat: Add `react-icons` and integrate `TiLocationArrow` in Hero component
    - Installed `react-icons` package (v5.5.0) and updated `package.json` and `package-lock.json`.
    - Created a reusable `Button` component with support for left and right icons.
    - Integrated the `Button` component into the `Hero` component.
    - Added `TiLocationArrow` icon from `react-icons` to the "Watch Trailer" button in the Hero section.
    - Adjusted the `Hero` component layout to include the new button and updated styles for better alignment.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react'
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -7,7 +9,7 @@ const Hero = () => {
   const [loading, setLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 4;
+  const totalVideos = 3;
   const nextVideoRef = useRef(null);
 
 
@@ -55,7 +57,7 @@ const Hero = () => {
 
           <video
             src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-            autoPlay
+            // autoPlay
             loop
             muted
             className='absolute left-0 top-0 size-full object-cover object-center'
@@ -65,7 +67,24 @@ const Hero = () => {
         <h1 className='special-font hero-heading absolute bottom-5 z-40 text-blue-100'>
           G<b>a</b>ming
         </h1>
+
+        <div className='absolute left-0 top-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
+
+            <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>Enter the Metagame Layer <br /> Unleash the Play Economy </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
+          </div>
+        </div>
       </div>
+      <h1 className='special-font hero-heading absolute bottom-5 text-black'>
+          G<b>a</b>ming
+      </h1>
     </div>
   )
 }
